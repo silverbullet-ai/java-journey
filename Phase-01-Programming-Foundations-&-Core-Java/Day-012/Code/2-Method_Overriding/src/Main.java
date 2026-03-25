6546540e9f81b405c13526d1040fb7b35a26c541
Silver Bullet <aahishsingh2000@outlook.com>
2026-03-25 21:48:13 +0530

    Day 12: Inheritance and Polymorphism - types, hierarchy, and method overriding

---
+++ Phase-01-Programming-Foundations-&-Core-Java/Day-012/Code/2-Method_Overriding/src/Main.java
@@ -0,0 +1,18 @@
+class Animal {
+    void sound() {
+        System.out.println("Animal sound");
+    }
+}
+
+class Cat extends Animal {
+    void sound() {
+        System.out.println("Meow");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Animal a = new Cat();
+        a.sound();
+    }
+}
