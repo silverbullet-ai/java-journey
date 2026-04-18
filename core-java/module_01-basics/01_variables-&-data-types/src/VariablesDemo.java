55187d39c2e07ba77d9a9749e0804848c2b3c9eb
Silver Bullet <aahishsingh2000@outlook.com>
2026-04-18 16:11:39 +0530

    core-java: initialize basics module with variables and data types (no…core-java: initialize basics module with variables and data types (notes + examples + practice)

---
+++ core-java/module_01-basics/01_variables-&-data-types/src/VariablesDemo.java
@@ -0,0 +1,10 @@
+public class VariablesDemo {
+    public static void main(String[] args) {
+
+        int age = 23;
+        String name = "Aahish";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
