e3c683f5ed2b046b1ba9d4771977dad4c32695e3
Silver Bullet <aahishsingh2000@outlook.com>
2026-04-27 18:43:50 +0530

    feat(core-java): add inheritance with single and multilevel examples

---
+++ core-java/module_02-oops/04_inheritance/src/InheritanceDemo.java
@@ -0,0 +1,21 @@
+class Animal {
+    void eat() {
+        System.out.println("Animal is eating");
+    }
+}
+
+class Dog extends Animal {
+    void bark() {
+        System.out.println("Dog is barking");
+    }
+}
+
+public class InheritanceDemo {
+    public static void main(String[] args) {
+
+        Dog d = new Dog();
+
+        d.eat();   // inherited
+        d.bark();  // own method
+    }
+}
