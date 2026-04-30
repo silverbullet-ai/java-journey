da1c558ece4731c649ea6ba5ef6e44c521ef61ee
Silver Bullet <aahishsingh2000@outlook.com>
2026-04-30 17:44:52 +0530

    feat(core-java): add ArrayList basics with operations and practice

---
+++ core-java/module_03-collections/01_arraylist/src/ArrayListDemo.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(10);
+        list.add(20);
+        list.add(30);
+
+        System.out.println(list);
+    }
+}
