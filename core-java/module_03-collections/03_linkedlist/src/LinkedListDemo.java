f00c581d51b5690d4fc92424114b622fda2d384e
Silver Bullet <aahishsingh2000@outlook.com>
2026-05-02 22:49:34 +0530

    feat(core-java): add LinkedList with operations and comparison to ArrayList

---
+++ core-java/module_03-collections/03_linkedlist/src/LinkedListDemo.java
@@ -0,0 +1,14 @@
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+    public static void main(String[] args) {
+
+        LinkedList<Integer> list = new LinkedList<>();
+
+        list.add(10);
+        list.add(20);
+        list.add(30);
+
+        System.out.println(list);
+    }
+}
