7d43a837050fd459765e95d4d78bc0c9129c5f78
Silver Bullet <aahishsingh2000@outlook.com>
2026-05-04 19:40:59 +0530

    feat(core-java): add stack and queue with basic operations and practice

---
+++ core-java/module_03-collections/04_stack-queue/src/QueueDemo.java
@@ -0,0 +1,18 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueDemo {
+    public static void main(String[] args) {
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.add(10);
+        queue.add(20);
+        queue.add(30);
+
+        System.out.println("Queue: " + queue);
+
+        System.out.println("Remove: " + queue.remove());
+        System.out.println("Peek: " + queue.peek());
+    }
+}
